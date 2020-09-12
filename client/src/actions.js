28c12b19d951632e389331d60e4927f7dea29c0f
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-09-12 14:55:49 -0400

    Finished Cart Reducer and Feed Reducer

---
+++ client/src/actions.js
@@ -0,0 +1,27 @@
+export const addItem = (item) => {
+  type: "ADD_ITEM", item;
+};
+
+export const removeItem = (id) => {
+  type: "REMOVE_ITEM", id;
+};
+
+export const updateQuantity = (id, newQuantity) => {
+  type: "UPDATE_QUANTITY", id, newQuantity;
+};
+
+export const clearCart = () => {
+  type: "CLEAR_CART";
+};
+
+export const requestItems = () => {
+  type: "REQUEST_ITEMS";
+};
+
+export const receiveItems = (items) => {
+  type: "RECEIVE_ITEMS", items;
+};
+
+export const catchError = (err) => {
+  type: "CATCH_ERROR", err;
+};
