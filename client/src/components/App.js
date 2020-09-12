a8773f5ab9822c59dfb99ee952f868f8c243238c
Kolby <mckeownkolby@gmail.com>
2020-09-12 11:32:51 -0400

    GlobalStyles and Colors Set

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import styled from "styled-components"
+import { COLORS } from './styles/Colors'
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   const [bacon, setBacon] = useState(null);
@@ -9,7 +12,18 @@ function App() {
       .then(data => setBacon(data));
   }, []);
 
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <>
+    <Test>Hi There</Test>
+    <div>{bacon ? bacon : `...where's my stuff?...`}</div>
+    <GlobalStyles />
+    </>
+  );
 }
+
+const Test = styled.p`
+  color: ${COLORS.PURPLE.PRIMARY};
+  font-size: 100px;
+`;
 
 export default App;
