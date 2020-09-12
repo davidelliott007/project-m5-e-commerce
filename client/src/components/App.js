a42b8c154f1540b166b9da9256375d4618575d77
Mae <maeberris@outlook.com>
2020-09-12 12:15:19 -0400

    Completed react Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [bacon, setBacon] = useState(null);
-
-  useEffect(() => {
-    fetch('/bacon')
-      .then(res => res.json())
-      .then(data => setBacon(data));
-  }, []);
-
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <Router>
+      {
+        //this is where the header component will be placed
+      }
+      <Switch>
+        <Route exact path="/">
+          {
+            //this is where the feed component will be placed
+          }
+          <div>test</div>
+        </Route>
+        <Route path="/item/:itemID">
+          <div>item</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
