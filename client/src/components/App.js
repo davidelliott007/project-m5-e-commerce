7f5ac617efd04b9e68cf2a076e45a153257230c7
Mae <maeberris@outlook.com>
2020-09-12 12:31:17 -0400

    Router tweaks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+import { COLORS } from "./styles/Colors";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       {
         //this is where the header component will be placed
       }
