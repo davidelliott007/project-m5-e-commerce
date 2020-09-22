044320c2e5763d3f52b5bc5cf0728dabdfe74dd0
Kolby <mckeownkolby@gmail.com>
2020-09-21 21:40:43 -0400

    Input Form

--- client/src/components/homepage/InputForm.js
+++ client/src/components/homepage/InputForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { COLORS } from "../styles/Colors";
+import { useSelector, useDispatch } from "react-redux";
+import { receiveItemsPaginated, receiveItems } from '../../actions'
 
 //We want:
 // 1. A dropdown menu that goes over each body part.
@@ -8,37 +10,44 @@ import { COLORS } from "../styles/Colors";
 //3. To display feed and pagination, we need to pass this filtered array to REDUX somehow (using daves new action)
 //const { receiveItemsPaginated } = require("../../actions");
 //Idea of how it would look
-// const FilterFunction = (BodyPart) => {
-//   return items.filter((item) => {
-//     if (item.body_location === BodyPart) {
-//       return true;
-//     }
-//   });
-// };
-// <form
-//   onChange={(ev) => {
-//     let selectedBodyPart = ev.target.value;
-//     let newItems = FilterFunction(selectedBodyPart);
-//     dispatch(receiveItemsPaginated(newItems));
-//   }}
-// ></form>;
 
 export const InputForm = () => {
-  const test = () => {
-    alert("test");
+  const dispatch = useDispatch()
+  const items = useSelector((state) => {
+    return state.feed.items.items;
+  });
+  const FilterFunction = (BodyPart) => {
+    return items.filter((item) => {
+      if (item.body_location === BodyPart) {
+        return true;
+      }
+    });
   };
-// wrist arms head waist chest hands feet
+
+  // wrist arms head waist chest hands feet
+
   return (
     <Wrapper>
       <Label for="bodyPart">Choose a body part!</Label>
-      <Select id="bodyPart" name="bodyPart">
-        <Option value="wrist">Wrist</Option>
-        <Option value="arms">Arms</Option>
-        <Option value="head">Head</Option>
-        <Option value="waist">Waist</Option>
-        <Option value="chest">Chest</Option>
-        <Option value="hands">Hands</Option>
-        <Option value="feet">Feet</Option>
+      <Select
+        id="bodyPart"
+        name="bodyPart"
+        onChange={(ev) => {
+          let selectedBodyPart = ev.target.value;
+          console.log(selectedBodyPart);
+          let newItems = FilterFunction(selectedBodyPart);
+          dispatch(receiveItemsPaginated({items: newItems}));
+        }}
+      >
+        <Option value="">Select an Option</Option>
+
+        <Option value="Wrist">Wrist</Option>
+        <Option value="Arms">Arms</Option>
+        <Option value="Head">Head</Option>
+        <Option value="Waist">Waist</Option>
+        <Option value="Chest">Chest</Option>
+        <Option value="Hands">Hands</Option>
+        <Option value="Feet">Feet</Option>
       </Select>
     </Wrapper>
   );
