0b8534e1e92b4b5ad1f30bde847bc3f705612f53
Mae <maeberris@outlook.com>
2020-09-23 21:03:31 -0400

    unstyled typeAhead

--- client/src/components/header/TypeAhead.js
+++ client/src/components/header/TypeAhead.js
@@ -1,12 +1,96 @@
 import React from "react";
-import styled from "styled-component";
+import styled from "styled-components";
+import { COLORS } from "../styles/Colors";
+import { useSelector } from "react-redux";
 
-const TypeAhead = ({ isOpened }) => {
+const TypeAhead = ({ maxHeight }) => {
+  let allItems = useSelector((state) => state.feed.items.items);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [selectedSuggestionIndex, setSelectedSuggestionIndex] = React.useState(
+    0
+  );
+  if (allItems === undefined) {
+    allItems = [];
+  }
+
+  let matchedItemsArray = allItems.filter((item) => {
+    let LowerCaseSearch = searchTerm.toLowerCase();
+    let itemLowerCaseName = item.name.toLowerCase();
+    if (
+      searchTerm !== "" &&
+      searchTerm.length >= 2 &&
+      itemLowerCaseName.includes(LowerCaseSearch)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  matchedItemsArray = matchedItemsArray.slice(0, 5);
+
+  console.log(matchedItemsArray);
+
+  let isMatchedItemsArrayFull = matchedItemsArray.length > 0;
   return (
-    <Wrapper>
-      <p>This is a test</p>
+    <Wrapper maxHeight={maxHeight}>
+      <InnerWrapper>
+        <Input
+          value={searchTerm}
+          onChange={(ev) => {
+            setSearchTerm(ev.target.value);
+          }}
+        />
+      </InnerWrapper>
+      <DropDown>
+        {matchedItemsArray.map((item, index) => {
+          let lowerCaseItemName = item.name.toLowerCase();
+          let lowerCaseSearchTerm = searchTerm.toLowerCase();
+          let indexOfSearch = lowerCaseItemName.indexOf(lowerCaseSearchTerm);
+          let indexToSlice = indexOfSearch;
+          let firstSlice = item.name.slice(0, indexToSlice);
+          let secondSlice = item.name.slice(indexToSlice);
+          return (
+            <li>
+              <span>
+                {`${firstSlice}`}
+                <Prediction>{`${secondSlice}`}</Prediction>
+              </span>
+            </li>
+          );
+        })}
+      </DropDown>
     </Wrapper>
   );
 };
+export default TypeAhead;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  transition: max-height 0.4s ease-in-out;
+  height: auto;
+  max-height: ${(props) => {
+    return props.maxHeight;
+  }};
+  overflow: hidden;
+  margin: 0;
+  width: 100%;
+`;
+
+const InnerWrapper = styled.div`
+  margin: 10px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  width: 30%;
+  border: 1px solid ${COLORS.PURPLE.PRIMARY};
+  height: 30px;
+  border-radius: 3px;
+`;
+
+const DropDown = styled.div``;
+
+const Prediction = styled.span`
+  font-weight: 700;
+`;
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -10,12 +10,18 @@ import { BiSearchAlt } from "react-icons/bi";
 // import { FaShoppingCart } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
 import { InputForm } from "../homepage/InputForm";
+import TypeAhead from "./TypeAhead";
 
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  let maxHeight = 0;
+  if (isOpen) {
+    maxHeight = "1000px";
+  }
+  const toggleSearchForm = () => setIsOpen(!isOpen);
   const [showForm, setShowForm] = useState(false);
   const displayForm = () => setShowForm(true);
   const hideForm = () => setShowForm(false);
@@ -27,13 +33,14 @@ const Header = () => {
         <A to="/">
           <ImgLogo src={imgLogo} />
         </A>
-        <A href="#" /* onClick to do some searching */>
+        <Button onClick={toggleSearchForm}>
           <BiSearchAlt size={35} />
-        </A>
+        </Button>
         <A to="/cart" /* onClick to go to cart / cart modal */>
           <FaShoppingCart size={35} />
         </A>
       </Nav>
+      <TypeAhead maxHeight={maxHeight} />
       <Input type="submit" value="Options" onClick={displayForm} />
       {showForm ? <InputForm /> : null}
       {showForm ? (
@@ -53,6 +60,7 @@ const Wrapper = styled.div`
   top: 0;
   border-bottom: 1px solid ${COLORS.BLUE.PRIMARY};
   z-index: 100;
+  transition: height 0.5ms;
 `;
 
 const Logo = styled.img`
@@ -81,4 +89,10 @@ const Input = styled.input`
   border-radius: 5px;
   border: none;
   margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  color: ${COLORS.PURPLE.PRIMARY};
+  background: none;
+  border: none;
 `;
